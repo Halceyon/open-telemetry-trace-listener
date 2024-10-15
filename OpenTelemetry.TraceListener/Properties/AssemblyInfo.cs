9431cb5e84009e5bba8a069adc3f030d4cc3cfe9
Halceyon <Halceyon@users.noreply.github.com>
2024-10-15 13:11:07 +0000

    Mainteinance/update namespace to singular (#5)
    * Refactor to OpenTelemetry.TraceListener
    
    Renamed project and files from `OpenTelemetry.TraceListeners` to `OpenTelemetry.TraceListener`. Updated build configurations, namespaces, and solution structure accordingly. Removed old configuration and package management files, indicating a modernization of the project setup.
    
    Introduced a new test project for `OpenTelemetry.TraceListener`:
    - Added `OpenTelemetry.TraceListener.Tests.csproj` with references to NUnit and other libraries.
    - Created `OpenTelemetryTraceListenerTests.cs` with several unit tests using NUnit.
    - Added `packages.config` to manage NuGet dependencies for the test project.
    
    * Refactor project files and remove OpenTelemetryTraceListener
    
    Refactored project files to address encoding and formatting issues:
    - Removed and re-added `Module.nuspec`, `OpenTelemetry.TraceListener.csproj`, and `app.config` files.
    - Corrected project path in `OpenTelemetryTraceListener.sln`.
    - Removed `OpenTelemetryTraceListener.cs`, `AssemblyInfo.cs`, and `packages.config`.
    
    * Update solution items in OpenTelemetryTraceListener.sln
    
    - Removed .github\workflows\Build-and-Test.yml from solution items.
    - Added .github\workflows\Build.yml to solution items.
    - Removed .github\workflows\Deploy.yml from solution items.
    - Added GitVersion.yml to solution items.

--- OpenTelemetry.TraceListener/Properties/AssemblyInfo.cs
+++ OpenTelemetry.TraceListener/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OpenTelemetry.TraceListeners")]
+[assembly: AssemblyTitle("OpenTelemetry.TraceListener")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OpenTelemetry.TraceListeners")]
+[assembly: AssemblyProduct("OpenTelemetry.TraceListener")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
