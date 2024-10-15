eb3cc6a41b27c09c72d18d6862b7a0d44c6307d0
Halceyon <Halceyon@users.noreply.github.com>
2024-10-15 12:00:49 +0000

    Initial-create (#1)

---
+++ OpenTelemetry.TraceListeners/OpenTelemetryTraceListener.cs
@@ -0,0 +1,148 @@
+﻿using System.Configuration;
+using System.Diagnostics;
+
+namespace OpenTelemetry.TraceListeners
+{
+    /// <summary>
+    /// Represents a trace listener that integrates with OpenTelemetry.
+    /// </summary>
+    public class OpenTelemetryTraceListener : TraceListener
+    {
+        /// <summary>
+        /// Writes trace information, including the message, to the listener specific output.
+        /// </summary>
+        /// <param name="eventCache">A <see cref="TraceEventCache"/> that contains the current process ID, thread ID, and stack trace information.</param>
+        /// <param name="source">The name of the trace source.</param>
+        /// <param name="eventType">One of the <see cref="TraceEventType"/> values specifying the type of event that has caused the trace.</param>
+        /// <param name="id">A numeric identifier for the event.</param>
+        /// <param name="format">A composite format string that contains text intermixed with zero or more format items, which correspond to objects in the <paramref name="args"/> array.</param>
+        /// <param name="args">An array containing zero or more objects to format.</param>
+        public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id, string format, params object[] args)
+        {
+            if (string.IsNullOrEmpty(format))
+            {
+                return;
+            }
+            var activityName = GetActivityName(source);
+            var message = format;
+
+            if ((args?.Length ?? 0) > 0)
+            {
+                message = string.Format(format, args);
+            }
+            CreateActivity(activityName, source, eventType, id, message, format, args);
+            return;
+        }
+
+        /// <summary>
+        /// Writes trace information, including the message, to the listener specific output.
+        /// </summary>
+        /// <param name="eventCache">A <see cref="TraceEventCache"/> that contains the current process ID, thread ID, and stack trace information.</param>
+        /// <param name="source">The name of the trace source.</param>
+        /// <param name="eventType">One of the <see cref="TraceEventType"/> values specifying the type of event that has caused the trace.</param>
+        /// <param name="id">A numeric identifier for the event.</param>
+        /// <param name="message">The trace message to write.</param>
+        public override void TraceEvent(TraceEventCache eventCache, string source, TraceEventType eventType, int id, string message)
+        {
+            var activityName = GetActivityName(source);
+            CreateActivity(activityName, source, eventType, id, message);
+            return;
+        }
+
+        /// <summary>
+        /// Writes a message to the listener specific output.
+        /// </summary>
+        /// <param name="message">The message to write.</param>
+        public override void Write(string message)
+        {
+            var activity = Activity.Current;
+            if (activity == null)
+            {
+                CreateActivity("log", "log", TraceEventType.Information, 0, message);
+                return;
+            }
+            activity.SetBaggage("message", message);
+            return;
+        }
+
+        /// <summary>
+        /// Writes a message followed by a line terminator to the listener specific output.
+        /// </summary>
+        /// <param name="message">The message to write.</param>
+        public override void WriteLine(string message)
+        {
+            var activity = Activity.Current;
+            if (activity == null)
+            {
+                CreateActivity("log", "log", TraceEventType.Information, 0, message);
+                return;
+            }
+            activity.SetBaggage("message", message);
+            return;
+        }
+
+        /// <summary>
+        /// Creates an activity and sets the necessary tags based on the provided parameters.
+        /// </summary>
+        /// <param name="activityName">The name of the activity.</param>
+        /// <param name="source">The name of the trace source.</param>
+        /// <param name="eventType">One of the <see cref="TraceEventType"/> values specifying the type of event that has caused the trace.</param>
+        /// <param name="id">A numeric identifier for the event.</param>
+        /// <param name="message">The trace message.</param>
+        /// <param name="format">A composite format string that contains text intermixed with zero or more format items, which correspond to objects in the <paramref name="args"/> array.</param>
+        /// <param name="args">An array containing zero or more objects to format.</param>
+        private void CreateActivity(string activityName, string source, TraceEventType eventType, int id, string message, string format = null, params object[] args)
+        {
+            using (var activity = GetActivitySource().StartActivity(activityName, ActivityKind.Client))
+            {
+                activity?.Start();
+                activity?.SetTag("source", source);
+                activity?.SetTag("message", message);
+                activity?.SetTag("eventType", eventType.ToString());
+                activity?.SetTag("id", id);
+                if (!string.IsNullOrEmpty(format))
+                {
+                    activity?.SetTag("format", format);
+                }
+                if (args?.Length > 0)
+                {
+                    activity?.SetTag("args", args);
+                }
+
+                if (eventType == TraceEventType.Error || eventType == TraceEventType.Critical || eventType == TraceEventType.Warning)
+                {
+                    activity?.SetStatus(ActivityStatusCode.Error);
+                }
+                else if (eventType == TraceEventType.Information)
+                {
+                    activity?.SetStatus(ActivityStatusCode.Ok);
+                }
+                else if (eventType == TraceEventType.Verbose)
+                {
+                    activity?.SetStatus(ActivityStatusCode.Unset);
+                }
+                activity?.Stop();
+            }
+        }
+
+        /// <summary>
+        /// Gets the activity name based on the provided source.
+        /// </summary>
+        /// <param name="source">The name of the trace source.</param>
+        /// <returns>The activity name.</returns>
+        private string GetActivityName(string source)
+        {
+            return string.Format("Log: {0}", source);
+        }
+
+        /// <summary>
+        /// Gets the activity source based on the provided configuration in OpenTelemetry:SourceName suffixed by ".Logs".
+        /// </summary>
+        /// <returns>The activity source.</returns>
+        private ActivitySource GetActivitySource()
+        {
+            var sourceName = ConfigurationManager.AppSettings["OpenTelemetry:SourceName"] ?? "Web";
+            return new ActivitySource($"{sourceName}.Logs");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,134 @@
-# open-telemetry-trace-listener
+# Open Telemetry Trace Listener
+
+This project is a trace listener for Open Telemetry, a set of APIs and libraries used for distributed tracing and observability in applications. The trace listener allows you to capture and send trace data to various backends for analysis and monitoring using your existing System.Diagnostics traces and logs.
+
+## Installation
+
+To use the Open Telemetry Trace Listener in your .NET Framework project, you can install it via NuGet.
+Simply run the following command in the NuGet Package Manager Console:
+
+```powershell
+Install-Package OpenTelemetry.TraceListener
+```
+
+## Usage with System.Diagnostics
+
+Add the trace listener to the system.diagnostics section of your App.config or Web.config file.
+
+```xml
+<configuration>
+  <system.diagnostics>
+    <trace>
+      <listeners>
+        <add name="OpenTelemetryTraceListener"
+             type="OpenTelemetry.TraceListener, OpenTelemetry.TraceListener"
+             initializeData="your-backend-endpoint" />
+      </listeners>
+    </trace>
+  </system.diagnostics>
+</configuration>
+```
+
+You can now use the System.Diagnostics.Trace class to log trace information. The Open Telemetry Trace Listener will capture these traces and send them to the configured backend.
+
+```cs
+using System.Diagnostics;
+
+public class Program
+{
+    public static void Main()
+    {
+        Trace.WriteLine("This is a trace message.");
+        Trace.TraceInformation("This is an informational message.");
+        Trace.TraceWarning("This is a warning message.");
+        Trace.TraceError("This is an error message.");
+    }
+}
+```
+
+## Example
+
+To set up logging your existing System.Diagnostics traces to a [Seq](https://datalust.co/seq) server, follow these steps:
+
+Install the Open Telemetry Nuget packages:
+
+```powershelll
+Install-Package OpenTelemetry.Exporter.Console
+Install-Package OpenTelemetry.Exporter.OpenTelemetryProtocol
+Install-Package OpenTelemetry.Instrumentation.AspNet -Version 1.9.0-beta.1
+Install-Package OpenTelemetry.Instrumentation.AspNet.TelemetryHttpModule -Version 1.9.0-beta.1
+Install-Package OpenTelemetry.TraceListener
+```
+
+Add the Trace Provider to the Application_Start event.
+
+```cs
+private TracerProvider _tracerProvider;
+
+protected void Application_Start(object sender, EventArgs e)
+{
+    var endpoint = ConfigurationManager.AppSettings["OpenTelemetry:EndPoint"] ?? "http://localhost:5341";
+    var apiKey = ConfigurationManager.AppSettings["OpenTelemetry:ApiKey"] ?? "";
+    var sourceName = ConfigurationManager.AppSettings["OpenTelemetry:SourceName"] ?? "Web";
+
+    _tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddAspNetInstrumentation()
+            .SetResourceBuilder(
+                ResourceBuilder.CreateDefault()
+                    .AddService(serviceName: sourceName))
+            .AddSource($"{sourceName}.Logs")
+            .AddConsoleExporter()
+            .AddOtlpExporter(options =>
+            {
+                options.Endpoint = new Uri($"{_endpoint}/ingest/otlp/v1/traces");
+                options.Protocol = OtlpExportProtocol.HttpProtobuf;
+                options.Headers = $"X-Seq-ApiKey={_apiKey}";
+            })
+        .Build();
+}
+```
+
+Add the shared listener to the system.diagnostics section of your web.config and assign the listener to the sources you want to log.
+
+```xml
+<system.diagnostics>
+    <sharedListeners>
+        <add name="OpenTelemetryTraceListener"
+            type="OpenTelemetry.TraceListener, OpenTelemetry.TraceListener"
+            initializeData="Example.Api" />
+    </sharedListeners>
+    <sources>
+        <source name="Bus" switchValue="All">
+            <listeners>
+                <clear />
+                <add name="sqldatabase" />
+                <add name="TelemetryListener" />
+            </listeners>
+        </source>
+        <source name="Web" switchValue="Information,Warning,Error,Critical">
+            <listeners>
+                <clear />
+                <add name="sqldatabase" />
+                <add name="TelemetryListener" />
+            </listeners>
+        </source>
+    </sources>
+    <trace autoflush="true" indentsize="0">
+        <listeners>
+            <clear />
+            <add name="OpenTelemetryTraceListener"
+                type="OpenTelemetry.TraceListener, OpenTelemetry.TraceListener"
+                initializeData="Example.Api" />
+        </listeners>
+    </trace>
+</system.diagnostics>
+```
+
+And then finally dispose the trace provider on application shutdown.
+
+```cs
+protected void Application_End(object sender, EventArgs e)
+{
+    _tracerProvider?.Dispose();
+}
+```
